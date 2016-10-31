4c9aa539b9150a7b96c83c326ba375ccef9f2b75
Antti Nykänen <ajjn@iki.fi>
2016-10-31 14:36:22 -0200

    root redirect to apigui

--- src/sandcage_django_example/urls.py
+++ src/sandcage_django_example/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+url(r'^admin/', admin.site.urls),
 
 urlpatterns = [
     url(r'^apigui/', include('apigui.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', lambda r: HttpResponseRedirect('apigui/')),
 ]
